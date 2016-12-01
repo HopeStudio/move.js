31ad08e1581d6f05cc9c0444097c943bc97cacca
yangfch3 <yangfch3@gmail.com>
2016-12-01 14:56:28 +0800

    init

---
+++ move.js
@@ -0,0 +1,313 @@
+var move = (function(window, undefined) {
+    "use strict";
+
+    /*
+     * Tween.js
+     * t: current time（当前时间）；
+     * b: beginning value（初始值）；
+     * c: change in value（变化量）；
+     * d: duration（持续时间）。
+     * you can visit 'http://easings.net/zh-cn' to get effect
+     */
+    Math.TWEEN = {
+        linear: function linear(t, b, c, d) {
+            return c * t / d + b;
+        },
+        easeInQuad: function easeInQuad(t, b, c, d) {
+            return c * (t /= d) * t + b;
+        },
+        easeOutQuad: function easeOutQuad(t, b, c, d) {
+            return -c * (t /= d) * (t - 2) + b;
+        },
+        easeInOutQuad: function easeInOutQuad(t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+            return -c / 2 * (--t * (t - 2) - 1) + b;
+        },
+        easeInCubic: function easeInCubic(t, b, c, d) {
+            return c * (t /= d) * t * t + b;
+        },
+        easeOutCubic: function easeOutCubic(t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t + 1) + b;
+        },
+        easeInOutCubic: function easeInOutCubic(t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+            return c / 2 * ((t -= 2) * t * t + 2) + b;
+        },
+        easeInQuart: function easeInQuart(t, b, c, d) {
+            return c * (t /= d) * t * t * t + b;
+        },
+        easeOutQuart: function easeOutQuart(t, b, c, d) {
+            return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+        },
+        easeInOutQuart: function easeInOutQuart(t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
+            return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+        },
+        easeInQuint: function easeInQuint(t, b, c, d) {
+            return c * (t /= d) * t * t * t * t + b;
+        },
+        easeOutQuint: function easeOutQuint(t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+        },
+        easeInOutQuint: function easeInOutQuint(t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t * t * t + b;
+            return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
+        },
+        easeInSine: function easeInSine(t, b, c, d) {
+            return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
+        },
+        easeOutSine: function easeOutSine(t, b, c, d) {
+            return c * Math.sin(t / d * (Math.PI / 2)) + b;
+        },
+        easeInOutSine: function easeInOutSine(t, b, c, d) {
+            return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
+        },
+        easeInExpo: function easeInExpo(t, b, c, d) {
+            return t == 0 ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
+        },
+        easeOutExpo: function easeOutExpo(t, b, c, d) {
+            return t == d ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
+        },
+        easeInOutExpo: function easeInOutExpo(t, b, c, d) {
+            if (t == 0) return b;
+            if (t == d) return b + c;
+            if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        },
+        easeInCirc: function easeInCirc(t, b, c, d) {
+            return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+        },
+        easeOutCirc: function easeOutCirc(t, b, c, d) {
+            return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
+        },
+        easeInOutCirc: function easeInOutCirc(t, b, c, d) {
+            if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
+            return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
+        },
+        easeInElastic: function easeInElastic(t, b, c, d, a, p) {
+            var s = void 0;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (typeof p == "undefined") p = d * .3;
+            if (!a || a < Math.abs(c)) {
+                s = p / 4;
+                a = c;
+            } else {
+                s = p / (2 * Math.PI) * Math.asin(c / a);
+            }
+            return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+        },
+        easeOutElastic: function easeOutElastic(t, b, c, d, a, p) {
+            var s = void 0;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (typeof p == "undefined") p = d * .3;
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else {
+                s = p / (2 * Math.PI) * Math.asin(c / a);
+            }
+            return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b;
+        },
+        easeInOutElastic: function easeInOutElastic(t, b, c, d, a, p) {
+            var s = void 0;
+            if (t == 0) return b;
+            if ((t /= d / 2) == 2) return b + c;
+            if (typeof p == "undefined") p = d * (.3 * 1.5);
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else {
+                s = p / (2 * Math.PI) * Math.asin(c / a);
+            }
+            if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+            return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 + c + b;
+        },
+        easeInBack: function easeInBack(t, b, c, d, s) {
+            if (typeof s == "undefined") s = 1.70158;
+            return c * (t /= d) * t * ((s + 1) * t - s) + b;
+        },
+        easeOutBack: function easeOutBack(t, b, c, d, s) {
+            if (typeof s == "undefined") s = 1.70158;
+            return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
+        },
+        easeInOutBack: function easeInOutBack(t, b, c, d, s) {
+            if (typeof s == "undefined") s = 1.70158;
+            if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= 1.525) + 1) * t - s)) + b;
+            return c / 2 * ((t -= 2) * t * (((s *= 1.525) + 1) * t + s) + 2) + b;
+        },
+        easeInBounce: function easeInBounce(t, b, c, d) {
+            return c - Tween.Bounce.easeOut(d - t, 0, c, d) + b;
+        },
+        easeOutBounce: function easeOutBounce(t, b, c, d) {
+            if ((t /= d) < 1 / 2.75) {
+                return c * (7.5625 * t * t) + b;
+            } else if (t < 2 / 2.75) {
+                return c * (7.5625 * (t -= 1.5 / 2.75) * t + .75) + b;
+            } else if (t < 2.5 / 2.75) {
+                return c * (7.5625 * (t -= 2.25 / 2.75) * t + .9375) + b;
+            } else {
+                return c * (7.5625 * (t -= 2.625 / 2.75) * t + .984375) + b;
+            }
+        },
+        easeInOutBounce: function easeInOutBounce(t, b, c, d) {
+            if (t < d / 2) {
+                return Tween.Bounce.easeIn(t * 2, 0, c, d) * .5 + b;
+            } else {
+                return Tween.Bounce.easeOut(t * 2 - d, 0, c, d) * .5 + c * .5 + b;
+            }
+        }
+    };
+
+    (function() {
+        var lastTime = 0;
+        var vendors = ['webkit', 'moz'];
+        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || // Webkit中此取消方法的名字变了
+                window[vendors[x] + 'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame) {
+            window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+                var id = window.setTimeout(function() {
+                    callback(currTime + timeToCall);
+                }, timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+        }
+        if (!window.cancelAnimationFrame) {
+            window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
+        }
+    })();
+
+    function getStyle(element, prop) {
+        if (prop) {
+            if (document.defaultView && document.defaultView.getComputedStyle) {
+                return document.defaultView.getComputedStyle(element, null)[prop];
+            } else if (element.currentStyle) {
+                return element.currentStyle[prop];
+            }
+            return element.style[prop];
+        }
+        if (document.defaultView && document.defaultView.getComputedStyle) {
+            return document.defaultView.getComputedStyle(element, null);
+        } else if (element.currentStyle) {
+            return element.currentStyle;
+        }
+        return element.style;
+    }
+
+    function extend(obj) {
+        for (var _len = arguments.length, rest = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+            rest[_key - 1] = arguments[_key];
+        }
+
+        if (rest.length > 0) {
+            if (Object.assign) {
+                return Object.assign.apply(Object, rest);
+            }
+            each(rest, function(restItem) {
+                each(restItem, function(value, key) {
+                    obj[key] = value;
+                });
+            });
+        }
+        return obj;
+    }
+
+    var noSuffix = ['opacity', 'backgroundSize'];
+
+    /**
+     * change 函数用于改变 ele 元素的样式
+     * @param ele
+     * @param step
+     *      width
+     *      height
+     *      opacity
+     *      left, right, top, bottom
+     *      margin, marginTop, marginBottom, marginLeft, marginRight
+     *      padding, paddingTop, paddingBottom, paddingLeft, paddingRight
+     *      border, borderTop, borderBottom, borderLeft, borderRight
+     *      backgroundSize
+     */
+    function change(ele, step) {
+        for (var prop in step) {
+            if (noSuffix.indexOf(prop) + 1) {
+                ele.style[prop] = step[prop];
+            } else {
+                ele.style[prop] = step[prop] + 'px';
+            }
+        }
+    }
+
+    // 写在 DOM 类的原型上，但是 IE8- 不支持
+    // HTMLElement.prototype.move = function(props, duration, complete) {
+    //     let ele;
+    //     if (this instanceof HTMLElement) {
+    //         ele = this;
+    //     } else {
+    //         return false;
+    //     }
+    function move(ele, props) {
+        var start = 0;
+        var during = Math.ceil(400 / 16.67);
+        var original = {};
+
+        var fx = Math.TWEEN.linear;
+        var complete = null;
+
+        for (var _len2 = arguments.length, rest = Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
+            rest[_key2 - 2] = arguments[_key2];
+        }
+
+        for (var index in rest) {
+            if (typeof rest[index] === 'number') {
+                during = Math.ceil(rest[index] / 16.67);
+            }
+            if (typeof rest[index] === 'string' && rest[index] in Math.TWEEN) {
+                fx = Math.TWEEN[rest[index]];
+            }
+            if (typeof rest[index] === 'function') {
+                complete = rest[index];
+            }
+        }
+
+        // 预处理 props 统一去掉单位
+        var processedProps = {};
+        for (var prop in props) {
+            if (typeof props[prop] === 'number' || /(\d+|\d*\.\d*)/.test(props[prop])) {
+                processedProps[prop] = parseFloat(props[prop]);
+                original[prop] = parseFloat(getStyle(ele, prop));
+            }
+        }
+
+        function exec() {
+            start++;
+            var step = {};
+            for (var _prop in processedProps) {
+                step[_prop] = fx(start, original[_prop], processedProps[_prop] - original[_prop], during);
+            }
+            change(ele, step);
+            if (start < during) {
+                requestAnimationFrame(exec);
+            }
+            if (start === during && typeof complete === 'function') {
+                complete();
+            }
+        }
+
+        exec();
+    }
+
+    return move;
+})(window, undefined);
+
+// 用法：move(element, propsObject[, duration][, fx][, completeCallback])
+// move($('#box'), {opacity: 0, width:'500px'}, 10000, 'easeInOutSine', function(){console.log('done')})
